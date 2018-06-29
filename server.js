09b1af6e3847c18f36c2176c8a12ffaea512ab03
Haritha Rathinakumar <hrathin@g.clemson.edu>
2018-06-29 11:16:50 -0400

    Add CORS support

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const Todo = require('./models');
 const app = express();
 app.use(bodyParser());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/todos', (req, res) => {
   Todo.all().then(todos => {
     res.send(todos);
